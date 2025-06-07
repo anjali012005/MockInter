67f0adf279ca725200470704412369e016eeaf8d
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-07 21:43:50 +0530

    Uncomment some functions

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
 import { getCurrentUser } from "@/lib/actions/auth.action";
+import { Toaster } from "sonner";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
 
@@ -14,11 +15,12 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">MockInter</h2>
         </Link>
       </nav>
 
       {children}
+      <Toaster/>
     </div>
   );
 };
