0ec337c706c793dcef1d20e13f0c7c518c08db50
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-07 22:02:12 +0530

    set-up firebase admin

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -3,27 +3,25 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 // Initialize Firebase Admin SDK
-function initFirebaseAdmin() {
+const  initFirebaseAdmin = () => {
   const apps = getApps();
-
   if (!apps.length) {
     initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace newlines in the private key
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+
       }),
+     
     });
   }
 
-  // return {
-  //   auth: getAuth(),
-  //   db: getFirestore(),
-  // };
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
 }
 
-export const db = getFirestore();
-export const auth = getAuth();
 
-// export const { auth, db } = initFirebaseAdmin();
+export const { auth, db } = initFirebaseAdmin();
