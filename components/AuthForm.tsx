e62f1eb9b1bab7f8bcba2af1a0aeb73cfb73025d
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-08 23:09:59 +0530

    changed some auth logic

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -41,26 +41,26 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        const { name, email, password } = data;
+        const { name, email, password } = values;
 
-        const userCredential = await createUserWithEmailAndPassword(
+        const userCredentials = await createUserWithEmailAndPassword(
           auth,
           email,
           password
         );
 
         const result = await signUp({
-          uid: userCredential.user.uid,
+          uid: userCredentials.user.uid,
           name: name!,
           email,
           password,
         });
 
-        if (!result.success) {
-          toast.error(result.message);
+        if (!result?.success) {
+          toast.error(result?.message);
           return;
         }
 
