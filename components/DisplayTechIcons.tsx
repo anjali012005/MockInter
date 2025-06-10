ebed85e040819584ff05b82e1aa15cf33f6cdad2
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-10 22:27:20 +0530

    fixed sign in err

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,34 +1,29 @@
-'use client'
-
-// import { useEffect, useState } from 'react';
-// import { cn, getTechLogos } from '@/lib/utils';
-// import Image from 'next/image';
+// 'use client'
+// import Image from "next/image";
+// import { cn, getTechLogos } from "@/lib/utils";
 
 // const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
-//   const [techIcons, setTechIcons] = useState([]);
-//   // const techIcons = await getTechLogos(techStack);
-  
-//   useEffect(() => {
-//     const fetchTechIcons = async () => {
-//       const icons = await getTechLogos(techStack);
-//       // setTechIcons(icons);
-//     };
-    
-//     fetchTechIcons();
-//   }, [techStack]); // Depend on techStack to refetch when it changes
+//   const techIcons = await getTechLogos(techStack);
 
 //   return (
-//     <div className='flex flex-row'>
+//     <div className="flex flex-row">
 //       {techIcons.slice(0, 3).map(({ tech, url }, index) => (
 //         <div
 //           key={tech}
 //           className={cn(
-//             'relative group bg-dark-300 rounded-full p-2 flex-center',
-//             index >= 1 && '-ml-3'
+//             "relative group bg-dark-300 rounded-full p-2 flex flex-center",
+//             index >= 1 && "-ml-3"
 //           )}
 //         >
-//           <span className='tech-tooltip'>{tech}</span>
-//           <Image src={url} alt={tech} width={100} height={100} className='size-5' />
+//           <span className="tech-tooltip">{tech}</span>
+
+//           <Image
+//             src={url}
+//             alt={tech}
+//             width={100}
+//             height={100}
+//             className="size-5"
+//           />
 //         </div>
 //       ))}
 //     </div>
@@ -37,13 +32,34 @@
 
 // export default DisplayTechIcons;
 
+// DisplayTechIcons.tsx
+
+
+'use client';
 
 import Image from "next/image";
-
 import { cn, getTechLogos } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
-const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
-  const techIcons = await getTechLogos(techStack);
+type TechIcon = {
+  tech: string;
+  url: string;
+};
+
+interface TechIconProps {
+  techStack: string[];
+}
+
+const DisplayTechIcons = ({ techStack }: TechIconProps) => {
+  const [techIcons, setTechIcons] = useState<TechIcon[]>([]); // ✅ specify type here
+
+  useEffect(() => {
+    const fetchIcons = async () => {
+      const icons = await getTechLogos(techStack);
+      setTechIcons(icons); // ✅ now this matches the expected type
+    };
+    fetchIcons();
+  }, [techStack]);
 
   return (
     <div className="flex flex-row">
