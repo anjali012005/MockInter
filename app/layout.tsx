3b80c228bb019638874dc37af360c7f8b32c11d9
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-08-18 00:28:37 +0530

    Edited layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const monaSans = Mona_Sans({
 
 export const metadata: Metadata = {
   title: "MockInter",
-  description: "An AI-powered platform for preparing for mock interviews",
+  description: "An AI-powered platform for preparing for mock interviews.",
 };
 
 export default function RootLayout({
