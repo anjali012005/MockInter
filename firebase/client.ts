21ccea7f8b7ff9f1a64a4ad1583a2ffcd2c3de09
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-12-07 10:42:59 +0530

    changed env var

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,18 +2,28 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKgLSbBV7FBlL2OqAjksFqzSmVSF3yMG8",
+//   authDomain: "mockinter-19bea.firebaseapp.com",
+//   projectId: "mockinter-19bea",
+//   // storageBucket: "mockinter-19bea.firebasestorage.app",
+//   storageBucket: "mockinter-19bea.appspot.com",
+//   messagingSenderId: "56014620588",
+//   appId: "1:56014620588:web:e87c1b6f6e79496d751c09",
+//   measurementId: "G-1QBPHW0XKB"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBKgLSbBV7FBlL2OqAjksFqzSmVSF3yMG8",
-  authDomain: "mockinter-19bea.firebaseapp.com",
-  projectId: "mockinter-19bea",
-  storageBucket: "mockinter-19bea.firebasestorage.app",
-  messagingSenderId: "56014620588",
-  appId: "1:56014620588:web:e87c1b6f6e79496d751c09",
-  measurementId: "G-1QBPHW0XKB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 
 export const auth = getAuth(app);
